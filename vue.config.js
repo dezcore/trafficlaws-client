97779294a587e1965cb3a405265a02ce202a7931
Dez <dez@MacBook-Pro-de-Dez.local>
2022-12-19 03:34:35 +0100

    test deployment (6)

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,17 @@
+/*module.exports = {
+  transpileDependencies: [
+    'vuetify'
+  ]
+}*/
+
+
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
   transpileDependencies: [
     'vuetify'
   ],
   publicPath: process.env.NODE_ENV === 'production' 
-  ? '/trafficlaws-client/' 
+  ? '/applications/' 
   : '/' 
 })
